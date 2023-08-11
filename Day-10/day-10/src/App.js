da5419508872b400755b8c432349127bbb5869cd
Nitesh Kumar <niteshkumar240198@gmail.com>
2023-08-11 15:35:56 +0530

    router

--- Day-10/day-10/src/App.js
+++ Day-10/day-10/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { Route, Routes, Link } from 'react-router-dom'
+
+import Home from './Pages/Home.jsx'
+import About from './Pages/About.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+      </div>
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+
+      </Routes>
+
+
     </div>
   );
 }
---
+++ Day-10/day-10/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return <div>
+
+        <h2>Welcome to Apni Website</h2>
+        <p>Explore about page to know more about website</p>
+    </div>
+
+}
